a2d895156136233fae1aeff5c34e90ffef7899c1
Chris Castle <github@crc.io>
2021-06-13 21:34:49 -0700

    Make hello world output more useful

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const http = require('http');
+const os = require('os');
 
 const PORT = process.env.PORT || 8080;
 
 const server = http.createServer((req, res) => {
-  res.write('Hello World!');
+  res.write('\nHello World!\n\n');
+  res.write(`(from ${os.hostname()} ${os.platform()} ${os.arch()})\n\n`)
   res.end();
 });
 
